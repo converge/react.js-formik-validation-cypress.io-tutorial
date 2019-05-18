150dee4049a31f4956e126e202d56e93b944147a
João Vanzuita <joaovanzuita@me.com>
2019-05-18 11:50:43 -0300

    adds Cypress tests, update React Class approach to Hooks

--- src/components/Login/index.jsx
+++ src/components/Login/index.jsx
@@ -1,83 +1,87 @@
-import React, { Component } from 'react'
+import React from 'react'
 import { Formik, Field, Form, ErrorMessage } from 'formik'
 import './style.css'
 
-export default class Login extends Component {
-
-  authUser = (username, password) => {
-    const result = (username === password) ? true : false
+function Login() {
+  // login and password validation
+  const authUser = (username, password) => {
+    const result = username === password ? true : false
     return result
   }
 
-  render() {
-    return (
-      <div className="login-box">
-        <div className="login-form">
-          <Formik
-            initialValues={{
-              username: '',
-              password: ''
-            }}
-            // validation fields
-            validate={ (values) => {
-              let errors = {}
-              if (!values.username) {
-                errors.username = 'Username is required'
-              } else if (!values.password) {
-                errors.password = 'Password is required'
-              }
-              return errors
-            }}
-            onSubmit={async (values, actions) => {
+  return (
+    <div className="login-box">
+      <div className="login-form">
+        <Formik
+          initialValues={{
+            username: '',
+            password: ''
+          }}
+          // validation fields
+          validate={values => {
+            let errors = {}
+            if (!values.username) {
+              errors.username = 'Username is required'
+            } else if (!values.password) {
+              errors.password = 'Password is required'
+            }
+            return errors
+          }}
+          onSubmit={async (values, actions) => {
+            actions.setStatus({
+              success: 'Trying to login...',
+              css: 'success'
+            })
+            actions.setSubmitting(false)
+            const response = authUser(values.username, values.password)
+            if (response) {
               actions.setStatus({
-                success: 'Trying to login...',
+                success: 'Credentials are correct !',
                 css: 'success'
               })
-              actions.setSubmitting(false)
-              const response = this.authUser(values.username, values.password)
-              if (response) {
-                actions.setStatus({
-                  success: 'Credentials are correct !',
-                  css: 'success'
-                })
-              } else {
-                  actions.setStatus({
-                    success: 'Username or Password incorrect !',
-                    css: 'error'
-                  })
-              }
-            }}
-            render={formikForm => (
-              <Form>
-                <div className="form-item">
-                  <Field name="username" type="text" placeholder="Username" />
-                </div>
-                <div className="form-item">
-                  <Field name="password" type="password" placeholder="Password" />
-                </div>
-                <ErrorMessage
-                  name="username"
-                  className="field-validation"
-                  component="div"
-                />
-                <ErrorMessage
-                  name="password"
-                  className="field-validation"
-                  component="div"
-                />
-                <div className={`form-sending ${formikForm.status ? formikForm.status.css : ''}`}>
-                  {formikForm.status ? formikForm.status.success : ''}
-                </div>
-                <div className="form-item">
-                  <button type="submit" disabled={formikForm.isSubmitting}>
-                    Login
-                  </button>
-                </div>
-              </Form>
-            )}
-          />
-        </div>
+            } else {
+              actions.setStatus({
+                success: 'Username or Password incorrect !',
+                css: 'error'
+              })
+            }
+          }}
+          render={formikForm => (
+            <Form>
+              <div className="form-item">
+                <Field name="username" type="text" placeholder="Username" />
+              </div>
+              <div className="form-item">
+                <Field name="password" type="password" placeholder="Password" />
+              </div>
+              <ErrorMessage
+                name="username"
+                className="field-validation"
+                component="div"
+              />
+              <ErrorMessage
+                name="password"
+                className="field-validation"
+                component="div"
+              />
+              <div
+                className={`form-sending ${
+                  formikForm.status ? formikForm.status.css : ''
+                }`}
+              >
+                {formikForm.status ? formikForm.status.success : ''}
+              </div>
+              <div className="form-item">
+                <button type="submit" disabled={formikForm.isSubmitting}>
+                  Login
+                </button>
+              </div>
+            </Form>
+          )}
+        />
       </div>
-    )
-  }
+    </div>
+  )
 }
+
+export default Login
